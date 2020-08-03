93ffff0186178db9970f0110d47e8776546b5e82
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-08-03 01:06:03 -0500

    Challenge 5 - Player ship Customization & UI

---
+++ Week 3/Challenge 5/Assets/ShipPartsDictionary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPartsDictionary : MonoBehaviour
+{
+    // NOTE: As written here, DICT could be modified by any script. Be careful with things like this.
+    public static Dictionary<ShipPart.eShipPartType, ShipPartsScriptableObject> DICT;
+    
+    //This allows all of the ShipPartsScriptableObjects to be assigned to this in the Inspector.
+    public ShipPartsScriptableObject[] shipPartSOs;
+
+    
+    void Awake()
+    {
+        // Create a Dictionary of ShipPartsScriptableObjects, which will make them easier to look up later
+        DICT=new Dictionary<ShipPart.eShipPartType, ShipPartsScriptableObject>();
+        foreach (var shipPartSO in shipPartSOs)
+        {
+            DICT.Add(shipPartSO.type,shipPartSO);
+        }
+    }
+}
